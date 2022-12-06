349a7bc6da7b72e58357f36c4278d9f5aa902fb7
LeonardMuia <leonardlouis25@outlook.com>
2022-12-06 18:30:25 +0300

    latest update

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,12 +1,12 @@
 <template>
     <header class="py-3 mb-4 border-bottom">
-        <div class="container d-flex flex-wrap justify-content-center">
+        <div class="container d-flex flex-wrap align-items-center">
           <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
             <span class="fs-4 h2 text-primary">Reviewed.ke</span>
           </a>
-          <form class="col-12 col-lg-auto mb-3 mb-lg-0">
-            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-          </form>
+          <div class="col-12 col-lg-auto mb-3 mb-lg-0">
+                <a class="btn btn-header shadow" href="#">For Businesses</a>
+          </div>
         </div>
     </header>
 </template>
