af8111967673eaa9d0f4be735b0ae33b7e516215
LeonardMuia <leonardlouis25@outlook.com>
2022-12-11 18:01:06 +0300

    latest update

--- resources/js/Components/CompanyContent.vue
+++ resources/js/Components/CompanyContent.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-8">
-                    <div class="card bg-light py-4">
+                    <div class="card bg-light py-4 rounded-5">
                         <div class="card-body text-center">
                             <div class="col-12 mb-2">
                                 <img class="img-fluid" src="/images/stars/zero.png" alt="">
@@ -16,12 +16,39 @@
                         </div>
                     </div>
 
-                    <div class="card bg-light mt-2">
-
+                    <div class="card bg-light mt-4 p-3 shadow rounded-5" v-for="review in reviews" :key="review">
+                        <div class="card-header py-3 align-items-center">
+                            <span class="avatar mr-2">
+                                <span class="avatar-text avatar-primary-light">{{getUser(review.user)}}</span>
+                            </span>
+                            <div class="user-info px-4">
+                                <div class="fw-bold">{{ review.user }}</div>
+                                <div class="span text-sm text-success" v-if="review.is_verified"><i class="uil uil-check-circle"></i> Verified Review</div>
+                                <div class="span text-sm text-danger" v-if="!review.is_verified"><i class="uil uil-times-circle"></i> Pending Verification</div>
+                            </div>
+                            <div class="rating">
+                                <img class="img-fluid" src="/images/stars/one.png" alt="Poor" v-if="review.rating == 1">
+                                <img class="img-fluid" src="/images/stars/two.png" alt="Bad" v-if="review.rating == 2">
+                                <img class="img-fluid" src="/images/stars/three.png" alt="Fair" v-if="review.rating == 3">
+                                <img class="img-fluid" src="/images/stars/four.png" alt="Good" v-if="review.rating == 4">
+                                <img class="img-fluid" src="/images/stars/five.png" alt="Excellent" v-if="review.rating == 5">
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="mt-3">
+                                <div class="h5 text-dark">{{ review.title }}</div>
+                                <div class="mt-3 text-secondary">
+                                    {{ review.review }}
+                                </div>
+                                <div class="mt-3 text-muted text-sm">
+                                    <span class="fw-bold">Date Posted:</span> <span class="text-dark">{{ getDate(review.created_at) }}</span> 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-4 sticky-top">
-                    <div class="card bg-light p-4">
+                    <div class="card bg-light p-4 rounded-5">
                         <div class="row text-secondary">
                             <div class="col-12">
                                 <div class="h4 text-dark">About {{ company[0].name }}</div>
@@ -54,11 +81,24 @@
 </template>
 
 <script>
+
+import moment from 'moment';
+
 export default {
     props: [
         'reviews',
         'company'
     ],
+
+    methods: {
+        getUser(user) {
+            return user.slice(0,2);
+        },
+
+        getDate(date){
+            return moment(date).format("Do MMMM YYYY [at] h:mm a");  
+        }
+    }
 }
 </script>
 
@@ -68,5 +108,23 @@ export default {
     }
     hr {
         background-color: #777;
+    }
+    .avatar {
+        display: inline-block;
+        position: relative;
+        width: 3rem;
+        height: 3rem;
+        text-align: center;
+        border-radius: 50%;
+        background-color: rgb(255, 201, 153, 0.7);
+        line-height: 3rem;
+    }
+    .card-header {
+        display: flex;
+        flex-direction: row;
+        background-color: #f8f9fa!important;
+    }
+    .rating {
+       margin-left: auto;
     }
 </style>
