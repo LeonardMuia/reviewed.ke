6712d319ce6a6ef54aac7ec8a677a3f8cae73473
LeonardMuia <leonardlouis25@outlook.com>
2022-12-11 05:23:07 +0300

    latest update

--- database/migrations/2022_12_11_020917_create_reviews_table.php
+++ database/migrations/2022_12_11_020917_create_reviews_table.php
@@ -15,6 +15,16 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('brands');
+            $table->integer('rating');
+            $table->longText('review');
+            $table->string('title');
+            $table->string('user')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('dating')->nullable();
+            $table->boolean('is_verified')->default(0);
             $table->timestamps();
         });
     }
